392cab9c76b4a0f300b7a0400d7234ad8bc616fa
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-08-22 23:47:04 +0600

    modifid a section

--- src/Pages/Home/ChefRecommends/ChefRecommends.jsx
+++ src/Pages/Home/ChefRecommends/ChefRecommends.jsx
@@ -4,9 +4,13 @@ import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 const ChefRecommends = () => {
     return (
         <div>
-            <SectionTitle heading={"CHef recommends"} subHeading={"Should Try"}/>
+            <SectionTitle 
+            heading={"CHef recommends"}
+             subHeading={"Should Try"}/>
+             //the rest will be added later on
         </div>
     );
 };
 
 export default ChefRecommends;
+
