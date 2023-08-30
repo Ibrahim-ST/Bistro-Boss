e2ff6db46b2c0f747a8431a12806a375c9418283
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-08-31 01:20:28 +0600

    added a parallax in menu cover

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
-import menuImg from '../../../assets/menu/menu-bg.jpg'
+import menuImg from '../../../assets/menu/menu-bg.png';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     
@@ -10,8 +11,12 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <Cover img={menuImg}/>
-          <h2>Menu</h2>  
+            <Cover img={menuImg} title={"our menu"}/>
+            <PopularMenu />
+            <Cover img={menuImg} title={"our menu"}/>
+            <PopularMenu />
+            <Cover img={menuImg} title={"our menu"}/>
+            <PopularMenu />
         </div>
     );
 };
