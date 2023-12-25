b46d32f873138ded72669d27faadd2ec4cc367b4
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-12-25 21:07:07 +0600

    added react tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,23 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <AuthProvider>
-        <div>
-          <RouterProvider router={router} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
       </AuthProvider>
     </HelmetProvider>
   </React.StrictMode>
